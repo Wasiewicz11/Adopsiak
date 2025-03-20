b35b89b9df43e4f0cc1aa446124d5f9a3ff9b9e0
Wasiewicz11 <filip.wasiewicz11@gmail.com>
2025-03-20 08:15:57 +0100

    init

--- Adopsiak.BILL/obj/Debug/net9.0/Adopsiak.BILL.AssemblyInfo.cs
+++ Adopsiak.BILL/obj/Debug/net9.0/Adopsiak.BILL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adopsiak.BILL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b3c148079cef15b970bc9fdcdb2de095425cdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adopsiak.BILL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adopsiak.BILL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Adopsiak.DAL/obj/Debug/net9.0/Adopsiak.DAL.AssemblyInfo.cs
+++ Adopsiak.DAL/obj/Debug/net9.0/Adopsiak.DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adopsiak.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b3c148079cef15b970bc9fdcdb2de095425cdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adopsiak.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adopsiak.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
